47c6c5dba105010b29bda96fa0a39631426e9b10
JSY-Ben <cheesypz@hotmail.com>
2025-12-30 21:01:52 +0000

    Fix Accordion hopefully!

--- staff/index.php
+++ staff/index.php
@@ -129,7 +129,7 @@ $logoPath = '../logo.png';
 $cssPath = '../style.css';
 $navActions = '<a class="btn btn-outline-secondary btn-sm" href="../index.php">Student View</a>'
     . '<a class="btn btn-outline-secondary btn-sm" href="../auth/logout.php">Logout</a>';
-$pageScripts = '<script src=\"https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js\"></script>';
+$pageScripts = '<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>';
 require __DIR__ . '/../header.php';
 ?>
 <main class="container py-4">
